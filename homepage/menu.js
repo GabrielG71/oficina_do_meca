8211d48c72b377e4e59fcd79cbe9c77627ca4a93
DanZx819 <147755543+DanZx819@users.noreply.github.com>
2024-10-07 21:58:11 -0300

    Correção do cabeçalho

--- homepage/menu.js
+++ homepage/menu.js
@@ -1,5 +1,13 @@
 
-window.addEventListener("scroll", function(){
-    let header = document.querySelector('#header')
-    header.classList.toggle('rolagem', window.scrollY > 0)
-})
+window.addEventListener("scroll", function() {
+    let header = document.querySelector('#header');
+    let main = document.querySelector('main');
+    
+    if (window.scrollY > 0) {
+        header.classList.add('rolagem');
+        main.style.marginTop = "120px"; // Ajusta quando o header encolhe
+    } else {
+        header.classList.remove('rolagem');
+        main.style.marginTop = "120px"; // Espaçamento normal
+    }
+});
